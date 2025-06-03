5c697793a8f9ad2deeea27adb1b17f68e77746f4
Rahul Sarkar <rnxart5798@gmail.com>
2025-06-03 13:51:41 +0530

    reverse an  integer

--- reverse_integer.java
+++ reverse_integer.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class reverse_integer {
+    public static void main(String[] args){
+        int a,b=0;
+        a=1234;
+        int temp=a;
+
+        while (temp!=0){
+            int rem = temp%10;
+            b= b*10+rem;
+            temp=temp/10;
+        }
+        System.out.print(b);    }
 }

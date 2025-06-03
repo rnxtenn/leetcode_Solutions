c33f658bb30f88014d7ba9fedb3c3b27d96df9ef
Rahul Sarkar <rnxart5798@gmail.com>
2025-06-03 20:54:13 +0530

    Modified Version

--- reverse_integer.java
+++ reverse_integer.java
@@ -1,13 +1,26 @@
-public class reverse_integer {
-    public static void main(String[] args){
-        int a,b=0;
-        a=1234;
-        int temp=a;
+class RevInt {
+    public int Revfuns(int a) {
+        int rev = 0;
+        int temp = a;
 
-        while (temp!=0){
-            int rem = temp%10;
-            b= b*10+rem;
-            temp=temp/10;
+        while (temp != 0) {
+            int rim = temp % 10;
+            if (rev > Integer.MAX_VALUE / 10 || rev < Integer.MIN_VALUE / 10){
+                System.out.print(0);
+                return 0;
+            }
+            rev = rev * 10+rim;
+            temp = temp / 10;
         }
-        System.out.print(b);    }
+        System.out.print("Reverse Integer is:"+rev);
+        return rev;
+    }
+}
+
+public class reverse_integer {
+    public static void main(String[] args) {
+        int a = 1534236469;
+        RevInt rv = new RevInt();
+        rv.Revfuns(a);
+    }
 }

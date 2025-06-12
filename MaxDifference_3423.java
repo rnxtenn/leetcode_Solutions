6486503af8da060a60a64d02e07df0430e843985
Rahul Sarkar <rnxart5798@gmail.com>
2025-06-12 16:10:31 +0530

    solution of 3423

--- MaxDifference_3423.java
+++ MaxDifference_3423.java
@@ -9,8 +9,9 @@ class Solution3423 {
     public int maxAdjacentDistance(int[] nums) {
         int n = nums.length;
         int maxDiff = 0;
-
+// loop helps us to iterate through the array.
         for (int i = 0; i < n; i++) {
+            // we use (i+1)%n instead of (i+1) beacuse it gives index 0 when i=3, that make it cyclic.
             int diff = Math.abs(nums[i] - nums[(i + 1) % n]);
             maxDiff = Math.max(maxDiff, diff);
         }
